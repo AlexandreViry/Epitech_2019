9fbb0d8962c458c3b62d4cdbbb9dac1329df00c1
Alexandre <alexandre.viry@trust-in-soft.com>
2020-08-12 17:01:34 +0200

    push 4

--- CPool_Day07_2019/my_strcat.c
+++ CPool_Day07_2019/my_strcat.c
@@ -5,23 +5,31 @@
 ** function that contatenate two strings
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 char *my_strcat (char *dest, char const *src) {
 
-    int d = 0;
-    
-    while (src[d] != '\0') {
-        d = d + 1;
-    }
-    int i = 0;
-        
-    while (dest[i] < '\0') {
-        i = i + 1;
-        char dest[d+i];
-    }
-    dest[i]=src[0];
-    while (src[i] < '\0') {
-        i = i+1;
-        dest[i]=src[d];
-    }
+    int size_src = strlen(src);
+    int size_dest = strlen(dest);
+    int end_of_line = size_src + size_dest + 1;
+
+    for (int i = 0; src[i] != '\0'; i++, size_dest++)
+        dest[size_dest] = src[i];
+    dest[end_of_line - 1] = '\0';
     return(dest);
+}
+
+int main(void)
+{
+    char *dest = malloc(11);
+    int res;
+
+    dest = strcpy(dest, "salut");
+    dest = my_strcat(dest, "salut");
+    printf("%s\n", dest);
+    res = strcmp(dest, "salutsalut");
+    //@ assert res == 0;
+    return 0;
 }
