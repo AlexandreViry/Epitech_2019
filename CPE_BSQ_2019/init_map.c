438afdbd66e948e59f6a31108882aed0af8c00a3
Alexandre <alexandre.viry@trust-in-soft.com>
2020-09-17 16:33:55 +0200

    greatest square

---
+++ CPE_BSQ_2019/init_map.c
@@ -0,0 +1,67 @@
+#include "include/my.h"
+
+void map_without_size(map_t **map)
+{
+    char *tmp = malloc(strlen((*map)->map) + 1);
+    int len = strlen((*map)->map);
+    int count;
+    char *tmp2;
+
+    strcpy(tmp, (*map)->map);
+    for (count = 0; (*map)->map[count] != '\n'; count++);
+    count++;
+    tmp2 = malloc(count + 1);
+    for (int i = 0; tmp[i] != '\n'; i++)
+        tmp2[i] = tmp[i];
+    tmp2[count - 1] = '\0';
+    (*map)->size_height = atoi(tmp2);
+    free(tmp2);
+    len = len - count;
+    (*map)->map = malloc(len + 1);
+    for (int i = 0; tmp[count] != '\0'; count++, i++)
+        (*map)->map[i] = tmp[count];
+    (*map)->map[len - 1] = '\0';
+}
+
+void fill_map(map_t **map)
+{
+    int x = 0;
+    int y = 0;
+
+    for (int i = 0; (*map)->map[i] != '\0'; i++, y++) {
+        if ((*map)->map[i] == '\n') {
+            (*map)->size_length = y;
+            x++;
+            y = -1;
+            continue;
+        }
+        if ((*map)->map[i] == '.')
+            (*map)->map_int[x][y] = 1;
+        if ((*map)->map[i] == 'o')
+            (*map)->map_int[x][y] = 0;
+    }
+}
+
+void init_map(char **filepath, map_t *map)
+{
+    struct stat sb;
+    int fd = open(filepath[1], O_RDONLY);
+    int count = 0;
+    char **tmp;
+
+    fstat(fd, &sb);
+    map->map = malloc(sizeof(char) * (int)sb.st_size + 1);
+    read(fd, map->map, sb.st_size);
+    map->map[sb.st_size] = '\0';
+    map_without_size(&map);
+    close(fd);
+    for (int i = 0; map->map[i] != '\0'; i++)
+        if (map->map[i] == '\n')
+            count++;
+    map->map_int = malloc(sizeof(int *) * (count + 1));
+    tmp = my_str_to_word_array(map->map, '\n');
+    for (count = 0; tmp[count] != NULL; count++) {
+        map->map_int[count] = malloc(sizeof(int) * (strlen(tmp[count]) + 1));
+    }
+    fill_map(&map);
+}
