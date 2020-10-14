4b202a625e829b4a634c005138376eb3f6ab86f1
Alexandre <alexandre.viry@trust-in-soft.com>
2020-10-14 14:46:35 +0200

    fix tis.config

--- CPool_bistro-matic_2019/tis_config_gen.sh
+++ CPool_bistro-matic_2019/tis_config_gen.sh
@@ -9,29 +9,29 @@ for f in ${list[@]};
 do
     echo "{
         \"files\": [
-            \"CPool_bistro-matic/division.c\",
-            \"CPool_bistro-matic/eval_expr.c\",
-            \"CPool_bistro-matic/infin_add.c\",
-            \"CPool_bistro-matic/infin_sub.c\",
-            \"CPool_bistro-matic/main2.c\",
-            \"CPool_bistro-matic/modulo.c\",
-            \"CPool_bistro-matic/my_str_to_word_array.c\",
-            \"CPool_bistro-matic/parentheses.c\",
-            \"CPool_bistro-matic/product.c\",
-            \"CPool_bistro-matic/src/analyse_string.c\",
-            \"CPool_bistro-matic/src/clear_string.c\",
-            \"CPool_bistro-matic/src/errors.c\",
-            \"CPool_bistro-matic/src/my_revstr.c\",
-            \"CPool_bistro-matic/src/power_of_ten.c\",
-            \"CPool_bistro-matic/src/my_strcat.c\"
+            \"CPool_bistro-matic_2019/division.c\",
+            \"CPool_bistro-matic_2019/eval_expr.c\",
+            \"CPool_bistro-matic_2019/infin_add.c\",
+            \"CPool_bistro-matic_2019/infin_sub.c\",
+            \"CPool_bistro-matic_2019/main2.c\",
+            \"CPool_bistro-matic_2019/modulo.c\",
+            \"CPool_bistro-matic_2019/my_str_to_word_array.c\",
+            \"CPool_bistro-matic_2019/parentheses.c\",
+            \"CPool_bistro-matic_2019/product.c\",
+            \"CPool_bistro-matic_2019/src/analyse_string.c\",
+            \"CPool_bistro-matic_2019/src/clear_string.c\",
+            \"CPool_bistro-matic_2019/src/errors.c\",
+            \"CPool_bistro-matic_2019/src/my_revstr.c\",
+            \"CPool_bistro-matic_2019/src/power_of_ten.c\",
+            \"CPool_bistro-matic_2019/src/my_strcat.c\"
         ],
         \"machdep\": \"gcc_x86_64\",
-        \"compilation_cmd\": \"-ICPool_bistro-matic\",
+        \"compilation_cmd\": \"-ICPool_bistro-matic_2019\",
         \"filesystem\": {
             \"files\": [
                 {
                     \"name\": \"inputs/input\",
-                    \"from\": CPool_bistro-matic/corpus/$f
+                    \"from\": CPool_bistro-matic_2019/corpus/$f
                 }
             ]
         }" >> tis.config
